9f1999f8c1e953358a691ab8ea77a7f53a562e07
ZunoSuninsqui <juanpisave27@gmail.com>
2024-07-30 16:36:34 -0500

    Changes by Usuario

--- UcobetAPP/Dominio/Sorteo.java
+++ UcobetAPP/Dominio/Sorteo.java
@@ -28,10 +28,6 @@ public class Sorteo {
     }
 
     public void agregarApuesta(Apuesta apuesta) {
-        // Que nos debe verificar primero?
-        //1 Que el numero no sea prohibido
-        //2 La fecha
-        //3 El tipo
         if(apuesta.getHoraCreacion().isBefore(LocalDateTime.from(this.horaJuego.minusMinutes(5)))) {
             if (!numerosProhibidos.contains(apuesta.getNumeroApostado())){
                 apuestasRecibidas.add(apuesta);
@@ -72,6 +68,26 @@ public class Sorteo {
             }
         }
     }
+
+    /*public void realizarSorteo() {
+        if (this.numeroGanador == null) {
+            System.out.println("El número ganador no ha sido escogido");
+            return;
+        }
+
+        System.out.println("El número ganador es: " + numeroGanador);
+
+        for (Apuesta apuesta : apuestasRecibidas) {
+            int matchCount = contarCoincidencias(apuesta.getNumeroApostado(), numeroGanador);
+            if (matchCount > 0) {
+                apuestasGanadoras.add(apuesta);
+                double premio = calcularPremio(apuesta.getCantidadApostada(), matchCount);
+                System.out.println("Premio ganado por " + apuesta.getUsuario().getNombre() + " es de: " + premio + " pesos.");
+            }
+        }
+        estimarRecaudo();
+    }*/
+
     public void mostrarApuestasTotales(){
         for (Apuesta ap : apuestasRecibidas){
             System.out.println();
@@ -84,6 +100,41 @@ public class Sorteo {
                .mapToDouble(Apuesta::getCantidadApostada)
                .sum();
     }
+
+    private int contarCoincidencias(String apostado, String ganador) {
+        int coincidencias = 0;
+        for (int i = 0; i < ganador.length(); i++) {
+            if (i < apostado.length() && apostado.charAt(i) == ganador.charAt(i)) {
+                coincidencias++;
+            }
+        }
+        return coincidencias;
+    }
+
+    private double calcularPremio(double cantidadApostada, int coincidencias) {
+        double multiplicador = 0;
+        switch (coincidencias) {
+            case 1:
+                multiplicador = 10;
+                break;
+            case 2:
+                multiplicador = 25;
+                break;
+            case 3:
+                multiplicador = 50;
+                break;
+            case 4:
+                multiplicador = 10000;
+                break;
+        }
+        return cantidadApostada * multiplicador;
+    }
+
+    /*public double estimarRecaudo() {
+        return apuestasRecibidas.stream()
+                .mapToDouble(Apuesta::getCantidadApostada)
+                .sum();
+    }*/
 
     public void finalizar(){this.finalizado = true;}
 
